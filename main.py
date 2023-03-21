39076f9c47bea09b3acc712e1eb5a532c846e70a
4dmin <marishka@outlook.ph>
2023-03-22 02:46:56 +0700

    morse code project done

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 
-# morse_code = {}
+morse_code_chart = {'A':'.-','B':'-...','C':'-.-.','D':'-..','E':'.','F':'..-.',
+            'G':'--.','H':'....','I':'..','J':'.---','K':'-.-',
+            'L':'.-..','M':'--','N':'-.','O':'---','P':'.--.','Q':'--.-',
+            'R':'.-.','S':'...','T':'-','U':'..-','V':'...-','W':'.--',
+            'X':'-..-','Y':'-.--','Z':'--..','1':'.----','2':'..---',
+            '3':'...--','4':'....-','5':'.....','6':'-....','7':'--...',
+            '8':'---..','9':'----.','0':'-----'}
 
 
-# message = input("Type the message to morse code: ")
+def convert_morse_code(text):
+    morse_code = ""
 
-test = "this is just a test"
+    if text.replace(' ', '').isalnum():
+        for char in text.replace(' ',''):
+            morse_code += morse_code_chart[char]+' '
+        print(morse_code)
+    else:
+        print("Only alphanumeric is allowed.")
 
-for char in test:
-    print(char)
+
+
+msg = input("Type the message to morse code: ").upper()
+
+convert_morse_code(msg)
